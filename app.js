b2d9d98e4369ef332390b8f9a5ab637099c918f1
Heikki Kurhinen <heikki.kurhinen@gmail.com>
2016-03-15 09:43:22 +0200

    ** Added basic frame for the application

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var expressSession = require('express-session');
+var cookieParser = require('cookie-parser');
+var config = require('./config');
+var flash = require('connect-flash');
+var passport = require('passport');
+
+mongoose.connect('mongodb://' + config.database.host + '/' + config.database.table);
+require('./auth/passport')(passport);
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(cookieParser());
+app.use(expressSession({secret:config.session_secret}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.set('port', config.port);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended : true
+}));
+
+require('./routes')(app);
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  database: {
+    host: 'localhost',
+    table: 'node-contacts'
+  },
+  session_secret: 'insertyoursecrethere',
+  server_root: '/contacts',
+}
