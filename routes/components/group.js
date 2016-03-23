c1cd21a0677f9b3b561ad4bd97395bed12cbffce
Heikki Kurhinen <heikki.kurhinen@gmail.com>
2016-03-23 10:49:24 +0200

    ** Added group route implementations

---
+++ routes/components/group.js
@@ -0,0 +1,62 @@
+var Group = require('../../model/group');
+var _ = require('underscore');
+   
+exports.create = function(req, res){
+  var group = new Group(req.body.group);
+  Group.save(function(err, group){
+    if(err){
+      res.status(400).send(err);
+    }else{
+      res.send(group);
+    }
+  });
+};
+
+exports.update = function(req, res){
+  var id = req.params.id;
+  Group.findById(id, function(err, group){
+    if(err){
+      res.status(400).send(err);
+    }else{
+      group = _.extend(group, req.body.group);
+      group.save(function(err, group){
+        if(err){
+          res.status(400).send(err);
+          res.send(group);
+        }
+      });
+    }
+  })
+}
+
+exports.find = function(req, res){
+  var id = req.params.id;
+  Group.findById(id, function(err, group){
+    if(err){
+      res.status(400).send(err);
+    }else{
+      res.send(group);
+    }
+  });
+}
+
+exports.list = function(req, res){
+  Group.find({}, function(err, groups){
+    if(err){
+      res.status(400).send(err);
+    }else{
+      res.send(groups);
+    }
+  });
+}
+
+exports.remove = function(req, res){
+  var id = req.params.id;
+  Group.findByIdAndRemove(id, function (err, group){
+      if(err){
+        res.status(400).send(err);
+      }else{
+        res.send(group);
+      } 
+  });
+}
